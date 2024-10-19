a613a78e0ea8738c6213afdfb87252e8c32cbffa
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2024-10-19 12:04:00 +0300

    [78] [Time Beats: 100.00%] [Memory Beats: 6.82%] - LeetPush

---
+++ 78-Subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result, solution = [], []
+
+        def backtrack(i: int):
+            if i >= len(nums):
+                return result.append(solution[:])
+            
+            solution.append(nums[i])
+            backtrack(i + 1)
+
+            solution.pop()
+            backtrack(i + 1)
+        backtrack(0)
+        return result
