80315ae929f20ad8b17d93d820b4b488f538252b
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2024-11-20 06:33:06 +0200

    [169] [Time Beats: 21.04%] [Memory Beats: 39.44%] - LeetPush

---
+++ 169-Majority-Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        return max(dic, key=dic.get)
