d920b47983cfd44575c11f7ff57cfd4d847af09e
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2024-12-22 11:41:19 +0200

    [804] [Time Beats: 100.00%] [Memory Beats: 7.32%] - LeetPush

---
+++ 804-Unique-Morse-Code-Words.py
@@ -0,0 +1,37 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        dic = {
+            "a": ".-",
+            "b": "-...",
+            "c": "-.-.",
+            "d": "-..",
+            "e": ".",
+            "f": "..-.",
+            "g": "--.",
+            "h": "....",
+            "i": "..",
+            "j": ".---",
+            "k": "-.-",
+            "l": ".-..",
+            "m": "--",
+            "n": "-.",
+            "o": "---",
+            "p": ".--.",
+            "q": "--.-",
+            "r": ".-.",
+            "s": "...",
+            "t": "-",
+            "u": "..-",
+            "v": "...-",
+            "w": ".--",
+            "x": "-..-",
+            "y": "-.--",
+            "z": "--..",
+        }
+        fres = []
+        for i in words:
+            res = []
+            for j in i:
+                res.append(dic[j])
+            fres.append("".join(res))
+        return len(set(fres))
