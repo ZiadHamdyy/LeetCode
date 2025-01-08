34efed13dca6d31093d56581ee5f4d39f2728846
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2025-01-08 06:23:00 +0200

    [48] [Time Beats: 100.00%] [Memory Beats: 8.99%] - LeetPush

--- 48-Rotate-Image.py
+++ 48-Rotate-Image.py
@@ -3,14 +3,4 @@ class Solution:
         \\\
         Do not return anything, modify matrix in-place instead.
         \\\
-        row = []
-        n = len(matrix)
-        for i in matrix:
-            for j in i:
-                row.append(j)
-        
-        for i in range(len(matrix)):
-            n = len(matrix) - i
-            for j in range(len(matrix[i])):
-                matrix[i][j] = row[-n]
-                n += len(matrix)
+        matrix[:] = zip(*matrix[::-1])
