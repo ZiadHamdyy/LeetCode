f7d6c756bda722ea3deeff29abc34ab05f129f59
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2025-02-01 12:38:41 +0200

    [3151] [Time Beats: 100.00%] [Memory Beats: 12.54%] - LeetPush

--- 3151-Special-Array-I.py
+++ 3151-Special-Array-I.py
@@ -3,8 +3,6 @@ class Solution:
         if len(nums) <= 1:
             return True
         for i in range(1, len(nums)):
-            if (nums[i] % 2 == 0 and nums[i - 1] % 2 == 0) or (
-                nums[i] % 2 != 0 and nums[i - 1] % 2 != 0
-            ):
+            if nums[i] % 2 == nums[i - 1] % 2:
                 return False
         return True
