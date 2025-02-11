5d2c9683313b1f98637be8c1f51b0b8880afaf1f
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2025-02-11 09:44:20 +0200

    [1910] [Time Beats: 30.24%] [Memory Beats: 6.21%] - LeetPush

--- DCP-02-25/1910-Remove-All-Occurrences-of-a-Substring.py
+++ DCP-02-25/1910-Remove-All-Occurrences-of-a-Substring.py
@@ -2,9 +2,10 @@ class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         stack = []
         n = len(part)
+        partL = list(part)
         for i in range(len(s)):
             stack.append(s[i])
-            if stack and stack[-n:] == list(part):
+            if stack and stack[-n:] == partL:
                 for _ in range(n):
                     stack.pop()
         return "".join(stack)
