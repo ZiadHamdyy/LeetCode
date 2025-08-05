dacf496e1e1c939d954952d11acd5d72c9f8e671
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2025-08-05 18:12:15 +0300

    Create 3477-Fruits-Into-Baskets-II.py

---
+++ 3477-Fruits-Into-Baskets-II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        for i in fruits:
+            for j in baskets:
+                if i <= j:
+                    baskets.remove(j)
+                    break
+                    
+        return len(baskets)
