0da8f2bf4c0ef01fb2399a5d4574b90f97db3407
Ziad Hamdy <133340500+ZiadHamdyy@users.noreply.github.com>
2025-08-11 22:14:07 +0300

    Create 2438-Range-Product-Queries-of-Powers.py

---
+++ 2438-Range-Product-Queries-of-Powers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        mod = int(1e9) + 7
+        s = (bin(n)[2:])[::-1]
+        arr = []
+
+        for x in range(len(s)):
+            if s[x] == "1":
+                if not arr:
+                    arr.append(2**x)
+                else:
+                    arr.append((2**x) * arr[-1])
+        ans = []
+        for q in queries:
+            temp_ans = arr[q[1]] // (arr[q[0]-1] if q[0] > 0 else 1)
+            ans.append(temp_ans % mod)
+        return ans
