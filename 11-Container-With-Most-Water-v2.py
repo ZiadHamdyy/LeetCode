9f6d54ad10faa2d73ad5813b7969b1a01382f2ec
Ziad Hamdy Abdelhady <133340500+ZiadHamdyy@users.noreply.github.com>
2025-10-04 14:24:06 +0300

    Implement maxArea method for container water problem
    Fix logic errors in maxArea calculation and pointer movement.

---
+++ 11-Container-With-Most-Water-v2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        l, r = 0, len(height) - 1
+        while l <= r:
+            maxArea = max(maxArea, min(height[l] * (r - l), height[r] * (r - l)))
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxArea
