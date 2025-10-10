e37fb4da87b04cd90e23c23fa65680075c0d2403
Ziad Hamdy Abdelhady <133340500+ZiadHamdyy@users.noreply.github.com>
2025-10-10 18:33:25 +0300

    Implement maximum energy calculation in Solution class

---
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        dp = energy
+        for i in range(len(energy) - k - 1, -1, -1):
+            dp[i] += dp[i + k]
+        return max(dp)
