d183ec8631e4d622877157ac9cbbe69aa7d47ca3
Ziad Hamdy Abdelhady <133340500+ZiadHamdyy@users.noreply.github.com>
2025-10-31 06:34:47 +0200

    [3289] [Time Beats: 27.35%] [Memory Beats: 20.87%] - LeetPush

---
+++ The-Two-Sneaky-Numbers-of-Digitville.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        res = []
+
+        for k, v in count.items():
+            if v == 2:
+                res.append(k)
+        return res
